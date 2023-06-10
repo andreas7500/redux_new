365327214b9c849f8f34fedcf20c014dc20fa3b9
Alexon7 <alenkaalexon@gmail.com>
2023-06-10 12:50:28 +0200

    add redux

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,68 +1,15 @@
-import { Component } from 'react';
-import { nanoid } from 'nanoid';
-import { useState, useEffect } from 'react';
+import TodoList from 'components/todoList/todoList';
 
-import {
-  Container,
-  Grid,
-  GridItem,
-  Header,
-  SearchForm,
-  Section,
-  Text,
-  Todo,
-} from 'components';
+import { Container, Header, SearchForm, Section } from 'components';
 
 export const App = () => {
-  const [todos, setTodos] = useState(
-    () => JSON.parse(localStorage.getItem('todos')) ?? []
-  );
-
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]);
-
-  const addTodo = text => {
-    const todo = {
-      id: nanoid(),
-      text,
-    };
-
-    setTodos([...todos, todo]);
-  };
-
-  const handleSubmit = data => {
-    addTodo(data);
-  };
-
-  const deleteTodo = id => {
-    setTodos(todos.filter(todo => todo.id !== id));
-  };
-
   return (
     <>
       <Header />
       <Section>
         <Container>
-          <SearchForm onSubmit={handleSubmit} />
-
-          {todos.length === 0 && (
-            <Text textAlign="center">There are no any todos ... </Text>
-          )}
-
-          <Grid>
-            {todos.length > 0 &&
-              todos.map((todo, index) => (
-                <GridItem key={todo.id}>
-                  <Todo
-                    id={todo.id}
-                    text={todo.text}
-                    counter={index + 1}
-                    onClick={deleteTodo}
-                  />
-                </GridItem>
-              ))}
-          </Grid>
+          <SearchForm />
+          <TodoList />
         </Container>
       </Section>
     </>
---
+++ src/redux/store.js
@@ -0,0 +1,33 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import rootReducer from './slice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
